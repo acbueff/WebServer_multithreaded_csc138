85a15de3c742737c0bcece62065fdd10c6782d1b
Andreas Bueff <acbueff@gmail.com>
2015-04-25 16:56:31 -0700

    corrected errors in webserver...socket now closes outside the loop clientloop

--- webserver2.py
+++ webserver2.py
@@ -13,13 +13,13 @@ serverSocket.listen(1)
 while True:
 	#Establish the connection
 	print 'Ready to serve...\n'
-	connectionSocket, addr = serverSocket.accept()
+	connectionSocket,addr = serverSocket.accept()
 	try:
 		message = connectionSocket.recv(2048)
 		filename = message.split()[1]
 		f = open(filename[1:])
 		outputdata = f.read()
-		print outputdata
+		#print outputdata
 		#SEND one HTTP header line into socket
 		connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
 		#Send the content of the requested file to the client
@@ -33,4 +33,4 @@ while True:
 		#Close clinet socket
 		connectionSocket.close()
 		
-	serverSocket.close()
+serverSocket.close()
